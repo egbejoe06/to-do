4678e21e797a5820bcdfe2de0c1457a47aca936b
Egbeleke Jonathan <bunkumijoe@gmail.com>
2023-10-06 05:51:07 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,303 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div :class="{ changebg: changebg, bg1: bg1 }">
+      <span class="todo">TODO</span>
+      <span @click="change()"> <img :src="'./assets/' + Image" alt="image" /> </span>
+    </div>
+    <div :class="{ bg2: bg2, changebg2: changebg2 }">
+      <div class="todos">
+        <div :class="{ tasks: tasks, tasks2: tasks2 }">
+          <input
+            v-model="check"
+            class="inp-cbx"
+            id="cbx-15"
+            type="checkbox"
+            style="display: none"
+          />
+          <label @click="addtask" class="cbx" for="cbx-15">
+            <span>
+              <svg width="12px" height="9px" viewbox="0 0 12 9">
+                <polyline points="1 5 4 8 11 1"></polyline>
+              </svg>
+            </span>
+          </label>
+          <span class="task"
+            ><input
+              v-model="Task"
+              @keyup.enter="addtask2()"
+              type="text"
+              placeholder="current typing"
+              :class="{ input: input, input2: input2 }"
+          /></span>
+        </div>
+        <div>
+          <All
+            :check="check"
+            :Task="Task"
+            :Todos="Todos"
+            :to_do="to_do"
+            :to_do2="to_do2"
+            :task1="task1"
+            :task2="task2"
+            @clear-completed="clearCompletedTodos"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import All from "./components/All.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      check: false,
+      Task: "",
+      Todos: [],
+      changebg: false,
+      changebg2: false,
+      bg1: true,
+      bg2: true,
+      tasks2: false,
+      tasks: true,
+      Image: "icon-sun.svg",
+      input: true,
+      input2: false,
+      to_do: true,
+      to_do2: false,
+      task1: true,
+      task2: false,
+    };
+  },
+  components: { All },
+  methods: {
+    addtask2() {
+      if (this.check == false && this.Task) {
+        this.Todos.push({ taask: this.Task, isChecked: false, completed: false });
+        this.Task = "";
+      }
+    },
+    addtask() {
+      if (this.check == false && this.Task) {
+        this.Todos.push({ taask: this.Task, isChecked: false, completed: false });
+        this.Task = "";
+        this.check = true;
+      }
+    },
+    change() {
+      this.changebg = !this.changebg;
+      this.bg1 = !this.bg1;
+      this.changebg2 = !this.changebg2;
+      this.bg2 = !this.bg2;
+      this.tasks = !this.tasks;
+      this.tasks2 = !this.tasks2;
+      this.input = !this.input;
+      this.input2 = !this.input2;
+      this.to_do = !this.to_do;
+      this.to_do2 = !this.to_do2;
+      this.task1 = !this.task1;
+      this.task2 = !this.task2;
+    },
+    clearCompletedTodos() {
+      this.Todos = this.Todos.filter((todo) => !todo.completed);
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+.input2 {
+  width: 220px;
+  background-color: transparent;
+  border: none;
+  padding: 8px;
+  color: hsl(235, 19%, 35%);
+  outline: none;
+}
+.tasks2 {
+  border-radius: 10px;
+  background-color: hsl(0, 0%, 98%);
+  width: 300px;
+  padding: 5px;
+}
+.changebg2 {
+  background-color: hsl(226.41deg 43% 95% / 74%);
+  width: 100%;
+  height: 100%;
+}
+.changebg {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 250px;
+  height: 150px;
+  width: 100%;
+  background-image: url(./assets/bg-desktop-light.jpg);
+}
+.tasks {
+  border-radius: 10px;
+  background-color: hsl(235, 21%, 11%);
+  width: 300px;
+  padding: 5px;
+}
+.task {
+  width: 100px;
+  padding: 10px;
+}
+.input {
+  width: 220px;
+  background-color: transparent;
+  border: none;
+  padding: 8px;
+  color: white;
+  outline: none;
+}
+.bg1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 250px;
+  height: 150px;
+  width: 100%;
+  background-image: url(./assets/bg-desktop-dark.jpg);
+}
+.bg2 {
+  background-color: hsl(235, 24%, 19%);
+  width: 100%;
+  height: 100%;
+}
+.todos {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  top: -45px;
+}
+.todo {
+  font-size: 30px;
+  color: hsl(0, 0%, 98%);
+}
+.inp-cbx {
+  display: none;
+}
+
+.cbx {
+  -webkit-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  cursor: pointer;
+}
+
+.cbx span {
+  display: inline-block;
+  vertical-align: middle;
+  transform: translate3d(0, 0, 0);
+}
+
+.cbx span:first-child {
+  position: relative;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  transform: scale(1);
+  vertical-align: middle;
+  border: 1px solid #b9b8c3;
+  transition: all 0.2s ease;
+}
+
+.cbx span:first-child svg {
+  position: absolute;
+  z-index: 1;
+  top: 8px;
+  left: 6px;
+  fill: none;
+  stroke: white;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-dasharray: 16px;
+  stroke-dashoffset: 16px;
+  transition: all 0.3s ease;
+  transition-delay: 0.1s;
+  transform: translate3d(0, 0, 0);
+}
+
+.cbx span:first-child:before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  background: #506eec;
+  display: block;
+  transform: scale(0);
+  opacity: 1;
+  border-radius: 50%;
+  transition-delay: 0.2s;
+}
+
+.cbx:hover span:first-child {
+  border-color: #3c53c7;
+}
+
+.inp-cbx:checked + .cbx span:first-child {
+  border-color: red;
+  background-color: hsl(234, 39%, 85%);
+  animation: check-15 0.6s ease;
+}
+
+.inp-cbx:checked + .cbx span:first-child svg {
+  stroke-dashoffset: 0;
+}
+
+.inp-cbx:checked + .cbx span:first-child:before {
+  transform: scale(2.2);
+  opacity: 0;
+  transition: all 0.6s ease;
+}
+
+.inp-cbx:checked + .cbx span:last-child {
+  color: #b9b8c3;
+  transition: all 0.3s ease;
+}
+
+.inp-cbx:checked + .cbx span:last-child:after {
+  transform: scaleX(1);
+  transition: all 0.3s ease;
+}
+
+@keyframes check-15 {
+  50% {
+    transform: scale(1.2);
+  }
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+@media (max-width: 768px) {
+  .main {
+    padding: 10px;
+  }
+
+  .changebg,
+  .todo {
+    font-size: 18px;
+  }
+
+  .tasks,
+  .tasks2 {
+    padding: 10px;
+  }
 }
 </style>
---
+++ src/components/All.vue
@@ -0,0 +1,239 @@
+<template>
+  <div>
+    <div class="bg11">
+      <div
+        :class="{ to_do: to_do, to_do2: to_do2 }"
+        v-for="(Todo, index) in filteredTodos"
+        :key="index"
+      >
+        <span class="todo-span">
+          <div class="checkbox_style">
+            <div class="checkbox-wrapper-18">
+              <div class="round">
+                <input
+                  type="checkbox"
+                  :id="'checkbox-' + index"
+                  v-model="Todo.isChecked"
+                  @change="toggleCompleted(index)"
+                />
+                <label :for="'checkbox-' + index"></label>
+              </div>
+            </div>
+          </div>
+          <li :class="{ task1: task1, task2: task2, completed: Todo.completed }">
+            {{ Todo.taask }}
+          </li>
+          <div class="cancel" @click="del(index)">
+            <img style="width: 15px" src="../assets/icon-cross.svg" alt="" />
+          </div>
+        </span>
+      </div>
+    </div>
+    <div class="footer">
+      <div>{{ filteredTodos.length }} items left</div>
+      <div class="footer1">
+        <div
+          class="f_all"
+          @click="fAll()"
+          :style="{ color: activeFilter === 'all' ? (to_do2 ? 'red' : 'blue') : '' }"
+        >
+          All
+        </div>
+        <div
+          class="f_active"
+          @click="fActive()"
+          :style="{ color: activeFilter === 'active' ? (to_do2 ? 'red' : 'blue') : '' }"
+        >
+          Active
+        </div>
+        <div
+          class="f_completed"
+          @click="fCompleted()"
+          :style="{
+            color: activeFilter === 'completed' ? (to_do2 ? 'red' : 'blue') : '',
+          }"
+        >
+          Completed
+        </div>
+      </div>
+      <div class="f_clear" @click="fClear()">Clear Completed</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["Todos", "check", "Task", "to_do", "to_do2", "task1", "task2"],
+  data() {
+    return {
+      filteredTodos: this.Todos,
+      activeFilter: "all",
+    };
+  },
+  watch: {
+    Todos(newTodos) {
+      // Update the filteredTodos data property whenever Todos changes
+      this.filteredTodos = newTodos.filter((todo) => !todo.completed);
+    },
+  },
+  methods: {
+    toggleCompleted(index) {
+      this.Todos[index].completed = !this.Todos[index].completed;
+    },
+    del(index) {
+      this.filteredTodos.splice(index, 1);
+    },
+    fActive() {
+      this.filteredTodos = this.Todos.filter((todo) => !todo.completed);
+      this.activeFilter = "active";
+    },
+    fCompleted() {
+      this.filteredTodos = this.Todos.filter((todo) => todo.completed);
+      this.activeFilter = "completed";
+    },
+    fAll() {
+      this.filteredTodos = this.Todos;
+      this.activeFilter = "all";
+    },
+    fClear() {
+      this.$emit("clear-completed");
+    },
+  },
+};
+</script>
+
+<style>
+.f_active,
+.f_all,
+.f_completed,
+.f_clear {
+  cursor: pointer;
+}
+.f_active:hover,
+.f_all:hover,
+.f_completed:hover,
+.f_clear:hover {
+  color: hsl(236, 33%, 92%);
+}
+.footer1 {
+  display: flex;
+  gap: 5px;
+}
+.to_do2 {
+  border-radius: 10px;
+  background-color: hsl(0, 0%, 98%);
+  width: 300px;
+  text-align: left;
+  padding-bottom: 5px;
+  padding: 5px;
+}
+.footer {
+  font-size: 13px;
+  display: flex;
+  gap: 20px;
+  margin-top: 20px;
+}
+.cancel {
+  position: relative;
+  left: 22px;
+  width: 15px;
+  top: 8px;
+  cursor: pointer;
+}
+.checkbox_style {
+  position: relative;
+  left: -7px;
+  top: 6px;
+}
+.task1 {
+  background-color: transparent;
+  border: none;
+  padding: 8px;
+  color: white;
+  outline: none;
+  font-size: 15px;
+  list-style: none;
+  overflow: auto;
+  width: 200px;
+}
+.task2 {
+  background-color: transparent;
+  border: none;
+  padding: 8px;
+  color: hsl(235, 19%, 35%);
+  outline: none;
+  font-size: 15px;
+  list-style: none;
+  overflow: auto;
+  width: 200px;
+}
+.to_do {
+  border-radius: 10px;
+  background-color: hsl(235, 21%, 11%);
+  width: 300px;
+  text-align: left;
+  padding-bottom: 5px;
+  padding: 5px;
+}
+
+.todo-span {
+  position: relative;
+  left: 20px;
+  display: flex;
+}
+
+.bg11 {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  position: relative;
+  left: 10px;
+}
+
+.checkbox-wrapper-18 .round {
+  position: relative;
+}
+.checkbox-wrapper-18 .round label {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 20px;
+  width: 20px;
+  display: block;
+}
+
+.checkbox-wrapper-18 .round label:after {
+  border: 2px solid #fff;
+  border-top: none;
+  border-right: none;
+  content: "";
+  height: 4px;
+  left: 4px;
+  opacity: 0;
+  position: absolute;
+  top: 6px;
+  transform: rotate(-45deg);
+  width: 8px;
+}
+
+.checkbox-wrapper-18 .round input[type="checkbox"] {
+  visibility: hidden;
+  display: none;
+  opacity: 0;
+}
+
+.checkbox-wrapper-18 .round input[type="checkbox"]:checked + label {
+  background-color: #66bb6a;
+  border-color: #66bb6a;
+}
+
+.checkbox-wrapper-18 .round input[type="checkbox"]:checked + label:after {
+  opacity: 1;
+}
+
+.completed {
+  text-decoration: line-through;
+  color: #888;
+}
+</style>
